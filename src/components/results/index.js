482c1297311c652008f1006681aa9983135f1543
islam rwashdeh <islamrwashdeh@yahoo.com>
2022-08-13 23:43:42 +0000

    phase 2 add

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -1,14 +1,15 @@
-import React from "react";
-import "./results.scss";
-
+import React from 'react';
+import './results.scss';
 export default function Results(props) {
   return (
-    <section>
-      <div className="box">
-        <pre>
-          {props.data ? JSON.stringify(props.data, undefined, 2) : null}
-        </pre>
+    <section >
+      <div className='divi'>
+        <p>Method: {props.method}</p>
+        <p> URL: {props.url}</p>
       </div>
-    </section>
+      <div className="box">
+        < pre > {props.data ? JSON.stringify(props.data, 100, 2) : null}</pre>
+      </div>
+    </section >
   );
 }
